1a529dc2e74ff7324dd3ff4d852984e7da90a3df
Alex Wilczewski <alexw77@gmail.com>
2013-07-12 00:14:31 -0400

    Reorganized activity functions to better match flow. Playback works while switching orientation.

--- src/com/example/AndroidVideoScan/MyActivity.java
+++ src/com/example/AndroidVideoScan/MyActivity.java
@@ -30,15 +30,16 @@ public class MyActivity extends Activity implements
     private static String TAG="AndroidVideoScan";
 
     private static String FILE = "/storage/emulated/0/DCIM/Camera/test1.mp4";
+    private static boolean LOADFILE = true;
 
-    private int width=0;
-    private int height=0;
-    private ScanningMediaPlayer player;
-    private VideoView surface;
+    private ScanningMediaPlayer mPlayer;
+    private VideoView mSurface;
     private SurfaceHolder holder;
-    private boolean playerPrepared;
 
-    private View mFullSize;
+    private boolean playerPrepared;
+    private boolean surfaceAvailable;
+
+    private ScalingView mFullSize;
     private ViewGroup mContextBar;
 
     private boolean seeking;
@@ -64,32 +65,39 @@ public class MyActivity extends Activity implements
         super.onCreate(savedInstanceState);
 
         playerPrepared = false;
+        surfaceAvailable = false;
 
         setContentView(R.layout.main);
 
         mFullSize = (ScalingView) findViewById(R.id.full_size);
-
         mContextBar = (ViewGroup) findViewById(R.id.context_bar);
+        mSurface = (VideoView) findViewById(R.id.videoviewsurface);
 
-        surface = (VideoView) findViewById(R.id.videoviewsurface);
-        holder = surface.getHolder();
+        holder = mSurface.getHolder();
         holder.addCallback(this);
-        holder.setFixedSize(1, 1);
 
-        Button btn = (Button) this.findViewById(R.id.select_file);
+//        mPlayer = new ScanningMediaPlayer();
+//        mPlayer.setOnPreparedListener(this);
+//        mPlayer.setOnSeekCompleteListener(this);
+//        mPlayer.setOnErrorListener(this);
+
+        Button btn = (Button) findViewById(R.id.select_file);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent chooseFile;
-                Intent intent;
-                chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
-                chooseFile.setType("file/*");
-                intent = Intent.createChooser(chooseFile, "Choose a file");
-                startActivityForResult(intent, ACTIVITY_CHOOSE_FILE);
+//                Intent chooseFile;
+//                Intent intent;
+//                chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+//                chooseFile.setType("file/*");
+//                intent = Intent.createChooser(chooseFile, "Choose a file");
+//                startActivityForResult(intent, ACTIVITY_CHOOSE_FILE);
+
+                beginPlaybackFromPath(FILE);
             }
         });
     }
 
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
@@ -103,23 +111,102 @@ public class MyActivity extends Activity implements
                     String filePath = uri.getPath();
                     Log.i(TAG, "PATH: "+filePath);
 
-                    setUpPlayer(filePath);
+                    beginPlaybackFromPath(filePath);
                 }
             }
         }
     }
 
+    // Surface Implementation
+    public void surfaceCreated(SurfaceHolder surfaceholder) {
+        Log.i(TAG, "MyActivity:surfaceCreated");
+
+        surfaceAvailable = true;
+
+        if(MyActivity.LOADFILE) {
+            beginPlaybackFromPath(FILE);
+        }
+    }
 
     public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
         // no-op
     }
 
     public void surfaceDestroyed(SurfaceHolder surfaceholder) {
-        // no-op
+        surfaceAvailable = false;
+    }
+    // End Surface Implementation
+
+    public void beginPlaybackFromPath(String path) {
+        Log.i(TAG, "path: "+path);
+
+        playerPrepared = false;
+
+        if(mPlayer != null) {
+            mPlayer.release();
+        }
+
+        try {
+            mPlayer = new ScanningMediaPlayer();
+
+            mPlayer.setOnPreparedListener(this);
+            mPlayer.setOnSeekCompleteListener(this);
+            mPlayer.setOnErrorListener(this);
+
+            mPlayer.setDataSource(path);
+
+            mPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
-    public void surfaceCreated(SurfaceHolder surfaceholder) {
-//        setUpPlayer();
+
+    // Player Implementations
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.i(TAG, "In On Prepared");
+
+        int width = mPlayer.getVideoWidth();
+        int height = mPlayer.getVideoHeight();
+
+        if (width != 0 && height != 0) {
+            playerPrepared = true;
+
+            int scrWidth = mFullSize.getMeasuredWidth();
+            int scrHeight = mFullSize.getMeasuredHeight();
+
+            resizeVideo(scrWidth, scrHeight);
+
+            mPlayer.setDisplay(holder);
+
+            seekPosition = mPlayer.getCurrentPosition();
+        }
+    }
+
+    public void onSeekComplete(MediaPlayer mediaPlayer) { }
+
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i2) {
+        Log.e(TAG, "Error 1: "+i);
+        Log.e(TAG, "Error 2: "+i2);
+        return false;
+    }
+    // End Player Implementations
+
+    public void resizeVideo(int width, int height) {
+        if(playerPrepared) {
+            int scrHeight = mFullSize.getHeight();
+            int ctxBarHeight = mContextBar.getHeight();
+            int maxHeight = scrHeight-ctxBarHeight;
+            Log.i(TAG, "MyActivity:resizeVideo - maxheight: "+maxHeight);
+            if(height > maxHeight) {
+                height = maxHeight;
+            }
+
+            IntegerPair p = fitPlayerToAspect(mPlayer, width, height);
+            Log.i(TAG, "MyActivity:resizeVideo - w:"+p.F);
+            Log.i(TAG, "MyActivity:resizeVideo - h:"+p.S);
+            directResize(p.F, p.S);
+        }
     }
 
     public IntegerPair fitPlayerToAspect(MediaPlayer mediaPlayer, float fitWidth, float fitHeight) {
@@ -144,88 +231,15 @@ public class MyActivity extends Activity implements
         return new IntegerPair(width, height);
     }
 
-    public void setUpPlayer(String path) {
-        Log.i(TAG, "In setUpPlayer");
-
-        playerPrepared = false;
-//        AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.test1);
-//        FileDescriptor fd = afd.getFileDescriptor();
-
-        try {
-            player = new ScanningMediaPlayer();
-
-//            player.setDisplay(holder);
-            player.setOnPreparedListener(this);
-            player.setOnSeekCompleteListener(this);
-            player.setOnErrorListener(this);
-//            player.setDataSource(fd, afd.getStartOffset(), afd.getLength());
-//            afd.close();
-            player.setDataSource(path);
-
-            player.prepareAsync();
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    public void onPrepared(MediaPlayer mediaplayer) {
-        Log.i(TAG, "In On Prepared");
-
-        width = player.getVideoWidth();
-        height = player.getVideoHeight();
-
-        if (width!=0 && height!=0) {
-            playerPrepared = true;
-
-            int scrWidth = mFullSize.getMeasuredWidth();
-            int scrHeight = mFullSize.getMeasuredHeight();
-
-            Log.i(TAG, "scrWidth: "+scrWidth);
-            Log.i(TAG, "scrHeight: "+scrHeight);
-
-            resizeVideo(scrWidth, scrHeight);
-
-            player.setDisplay(holder);
-
-            seekPosition = player.getCurrentPosition();
-        }
-    }
-
-    public void resizeVideo(int width, int height) {
-        if(playerPrepared) {
-            int scrHeight = mFullSize.getHeight();
-            int ctxBarHeight = mContextBar.getHeight();
-            int maxHeight = scrHeight-ctxBarHeight;
-            Log.i(TAG, "MyActivity:resizeVideo - maxheight: "+maxHeight);
-            if(height > maxHeight) {
-                height = maxHeight;
-            }
-
-            IntegerPair p = fitPlayerToAspect(player, width, height);
-            Log.i(TAG, "MyActivity:resizeVideo - w:"+p.F);
-            Log.i(TAG, "MyActivity:resizeVideo - h:"+p.S);
-            holder.setFixedSize(p.F, p.S);
-        }
-    }
-
     public void directResize(int width, int height) {
         if(playerPrepared) {
             holder.setFixedSize(width, height);
         }
     }
 
-    public void onSeekComplete(MediaPlayer mediaPlayer) { }
-
     public void scan(float seek) {
         seeking = true;
-        seekPosition = (int) (seek*1000 + player.getCurrentPosition());
-        player.seekTo(seekPosition);
-    }
-
-    @Override
-    public boolean onError(MediaPlayer mediaPlayer, int i, int i2) {
-        Log.e(TAG, "Error 1: "+i);
-        Log.e(TAG, "Error 2: "+i2);
-        return false;    // Error -38 lol
+        seekPosition = (int) (seek*1000 + mPlayer.getCurrentPosition());
+        mPlayer.seekTo(seekPosition);
     }
 }
